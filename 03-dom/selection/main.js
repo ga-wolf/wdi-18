e7e28a2ea944693a78b110a9e172bd7a6618c1d3
ga-wolf <jack.jeffress@ga.co>
2016-11-02 11:02:27 +1100

    Added DOM selection

---
+++ 03-dom/selection/main.js
@@ -0,0 +1,10 @@
+// document.getElementById( idAsString );
+
+// Traverse the Document Object Model tree
+  // When you find an element with the ID of mainHeading
+  // Save it in a variable called heading
+
+var heading = document.getElementById("mainHeading");
+var importantParagraph = document.getElementById("important");
+
+console.log( heading, importantParagraph );
