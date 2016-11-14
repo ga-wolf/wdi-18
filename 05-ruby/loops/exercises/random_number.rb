1616d9ac0106881ada0bf58d5dafb288d2bd7f95
cjbarnaby <cjbarnaby@gmail.com>
2016-11-14 15:02:38 +1100

    random number

--- 05-ruby/loops/exercises/random_number.rb
+++ 05-ruby/loops/exercises/random_number.rb
@@ -1,7 +1,20 @@
-comp_number = Random.rand( 0..10 )
+print "What is the maximum for the game?"
+
+max = gets().to_i()
+
+comp_number = Random.rand( max )
 
 print "What is your guess? "
 user_guess = gets().to_i()
 
 # I want to keep asking the user for input until the user_guess is the same as comp_number
-  
+while user_guess != comp_number
+  if user_guess < comp_number
+    puts "Wrong. Guess higher."
+  else
+    puts "Wrong. Guess lower."
+  end
+  user_guess = gets().to_i()
+end
+
+puts "Congratulations. #{comp_number} was the number."
