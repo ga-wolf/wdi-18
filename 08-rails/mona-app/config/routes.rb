c57d52b52d10915ad54472fab481b0f499b19bf8
ga-wolf <jack.jeffress@ga.co>
2016-11-22 10:47:16 +1100

    Added home and about page

---
+++ 08-rails/mona-app/config/routes.rb
@@ -0,0 +1,14 @@
+# == Route Map
+#
+# Prefix Verb URI Pattern     Controller#Action
+#   root GET  /               pages#home
+#   home GET  /home(.:format) pages#home
+#
+
+Rails.application.routes.draw do
+
+  root "pages#home"
+  get "/home" => "pages#home"
+  get "/about" => "pages#about"
+
+end
