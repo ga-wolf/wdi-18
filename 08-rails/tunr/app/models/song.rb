5baafc1732bbb0dd621b10f99496868d1fadd14e
cjbarnaby <cjbarnaby@gmail.com>
2016-11-23 15:55:25 +1100

    merge

---
+++ 08-rails/tunr/app/models/song.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  name       :text
+#  artist_id  :integer
+#  album_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Song < ActiveRecord::Base
+  belongs_to :artist
+end
