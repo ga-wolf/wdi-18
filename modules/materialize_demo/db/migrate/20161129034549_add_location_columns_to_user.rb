ab2ed3435d1df8496668bee425522f30d9c919d6
cjbarnaby <cjbarnaby@gmail.com>
2016-11-30 15:04:00 +1100

    added cloudinary

---
+++ modules/materialize_demo/db/migrate/20161129034549_add_location_columns_to_user.rb
@@ -0,0 +1,7 @@
+class AddLocationColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :address, :string
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+  end
+end
