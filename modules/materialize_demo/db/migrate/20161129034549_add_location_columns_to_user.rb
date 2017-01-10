3c632e482bf520be2f6dad22c107a1172e735baf
cjbarnaby <cjbarnaby@gmail.com>
2017-01-10 16:13:27 +1100

    removed junk

--- modules/materialize_demo/db/migrate/20161129034549_add_location_columns_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddLocationColumnsToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :address, :string
-    add_column :users, :latitude, :float
-    add_column :users, :longitude, :float
-  end
-end
